5d8debac62bd95168d427897639331d9bea67e0c
KarolByczek <byczek_1986@interia.pl>
2024-09-05 12:12:22 +0200

    First commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/PROJECT_NO2_EMPLOYEE_MANAGER/'  // Replace with your repo name
+})
