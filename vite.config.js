6601d4ba6e4480e23312236f6595f4da8bf57a5e
KarolByczek <byczek_1986@interia.pl>
2024-09-05 12:40:27 +0200

    Setting the chunk size limit.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PROJECT_NO2_EMPLOYEE_MANAGER/'  // Replace with your repo name
+  base: '/PROJECT_NO2_EMPLOYEE_MANAGER/',
+  build: {
+    chunkSizeWarningLimit: 1000 // set this to your desired limit in KB
+  },
 })
