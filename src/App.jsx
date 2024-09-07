f6a675206f3cc32cb0b2fca4ce57c8d352fbe66a
KarolByczek <byczek_1986@interia.pl>
2024-09-07 18:41:32 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
 import { AddEmployee } from './pages/AddEmployee';
 import { EditEmployee } from './pages/EditEmployee';
@@ -9,14 +9,14 @@ const basename = "/PROJECT_NO2_EMPLOYEE_MANAGER";
 
 function App() {
   return (
-    <Router basename={basename}>
+    <HashRouter basename={basename}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/add_form" element={<AddEmployee />} />
         <Route path="/edit_page" element={<EditEmployee />} />
         <Route path="/details" element={<EmployeeDetail />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
